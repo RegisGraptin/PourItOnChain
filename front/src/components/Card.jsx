8c387f31e7b2ba32001627479d0543f849407550
rere <regis-graptin@sfr.fr>
2024-11-15 23:25:48 +0700

    feat: wine

--- front/src/components/Card.jsx
+++ front/src/components/Card.jsx
@@ -1,36 +1,24 @@
-"use client";
-
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-  } from "@material-tailwind/react";
-   
-  export default function CardDefault() {
+     export default function CardDefault() {
     return (
-      <Card className="mt-6 w-96">
-        <CardHeader color="blue-gray" className="relative h-56">
-          <img
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-            alt="card-image"
-          />
-        </CardHeader>
-        <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            UI/UX Review Check
-          </Typography>
-          <Typography>
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-          </Typography>
-        </CardBody>
-        <CardFooter className="pt-0">
-          <Button>Read More</Button>
-        </CardFooter>
-      </Card>
+      <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+      <div className="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
+        <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80" alt="card-image" />
+      </div>
+      <div className="p-4">
+        <h6 className="mb-2 text-slate-800 text-xl font-semibold">
+          Website Review Check
+        </h6>
+        <p className="text-slate-600 leading-normal font-light">
+          The place is close to Barceloneta Beach and bus stop just 2 min by walk
+          and near to &quot;Naviglio&quot; where you can enjoy the main night life in
+          Barcelona.
+        </p>
+      </div>
+      <div className="px-4 pb-4 pt-0 mt-2">
+        <button className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+          Read more
+        </button>
+      </div>
+    </div>  
     );
   }
