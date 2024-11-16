46eed04a48f6c1a9933f51945c070bbc350341e2
rere <regis-graptin@sfr.fr>
2024-11-16 09:23:02 +0700

    deploy & verify with blockscout

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 code deploy: 0xaE5Dd577835069e6686963C4d36acb7CFe87De04.
 scroll sepolia
+
+
+
+Note blockscout 
+On the endpoint needed to do: '?module=contract&action=verify'
+
+ "https://scroll-sepolia.blockscout.com/api?module=contract&action=verify"
+
+
+Note that the url in forge is not the same verified
+Input url should be: https://scroll-sepolia.blockscout.com
+
+https://scroll-sepolia.blockscout.com/address/0xf7d25eb89fC8D1530c4304CCbF57922419f0BCF9
--- contract/deploy.sh
+++ contract/deploy.sh
@@ -1,3 +1,10 @@
 source .env
 
-forge script --chain scroll script/Wine.s.sol:WineScript --rpc-url $SCROLL_RPC_URL --broadcast --verify --verifier blockscout --verifier-url https://scroll-sepolia.blockscout.com/api?module=contract&action=verify -vvvv
+forge script \
+  --rpc-url $SCROLL_RPC_URL \
+  --private-key $PRIVATE_KEY \
+  script/Wine.s.sol:WineScript \
+  --verify \
+  --verifier blockscout \
+  --verifier-url https://scroll-sepolia.blockscout.com/api/ \
+  --broadcast
