14a46c9a4d16525173e02e1011f275c02f047dfb
Riki0923 <83667276+Riki0923@users.noreply.github.com>
2024-11-16 23:22:37 +0100

    remove getCircleSDK

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import ShowcaseWine from '../components/ShowcaseWine';
-import { useState } from "react";
-import { getCircleSdk } from "../utils/circleSdk";
 import Header from '../components/Header';
 
 const Home: NextPage = ()  => {
